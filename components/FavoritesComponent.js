926283801f5a247b04cef7188e94c2f799ce2a1c
brucker68 <rucker_bennie@yahoo.com>
2020-02-25 07:44:40 -0600

    Unable to resolve "redux" from "node_modules\react-redux\lib\connect\mapDispatchToProps.js"
    changes same blue screen

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { FlatList, View, Text } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
-//import { Loading } from './LoadingComponent';
+import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 
 const mapStateToProps = state => {
@@ -32,7 +32,7 @@ class Favorites extends Component {
         };
 
         if (this.props.campsites.isLoading) {
-           // return <Loading />;
+            return <Loading />;
         }
         if (this.props.campsites.errMess) {
             return (
